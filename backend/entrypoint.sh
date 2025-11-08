d14564ea72bbc5a524cc5fdcc0440e5b0810f6c2
Lucas Santana <lucas.dsousast@gmail.com>
2025-11-07 23:17:26 -0300

    Fix entrypoint to LF

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 echo "O Docker Compose aguardou o PostgreSQL ficar pronto."
 
-# Aplica as migrações do banco de dados
 echo "Aplicando migrações do banco de dados..."
 python manage.py migrate --noinput
 
-# Inicia o servidor
 python manage.py runserver 0.0.0.0:8000
